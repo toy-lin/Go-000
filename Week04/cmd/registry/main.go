8beadfe85a6023ed859fbbe0dfd54182dbc9ef88
linkt2018 <1650476351@qq.com>
2020-12-22 00:08:01 +0800

    update

--- Week04/cmd/registry/main.go
+++ Week04/cmd/registry/main.go
@@ -1,17 +1,39 @@
 package registry
 
 import (
-    "Week04/api/registry"
-    "fmt"
-    "github.com/gin-gonic/gin"
+	"Week04/api/registry"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+	"os/signal"
 )
 
 func main() {
-    e := gin.New()
+	e := gin.New()
 
-    registry.RegisterLoginHTTPServer(e)
+	registry.RegisterLoginHTTPServer(e)
 
-    if err := e.Run(":8000"); err != nil {
-        fmt.Println(err)
-    }
+	Go(func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		if e != nil {
+
+		}
+	})
+
+	if err := e.Run(":8000"); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Go(f func()) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println(r)
+			}
+		}()
+		f()
+	}()
 }
